8eb58df18b23dcc7a8ae3dbd34841ad3e0d08141
BrianGossett <brian.gossett.usa@gmail.com>
2022-08-08 10:30:42 -0400

    Added top bar

---
+++ src/componets/topbar/index.js
@@ -0,0 +1,15 @@
+import "./TopBar.css";
+
+function TopBar() {
+
+
+    return(
+        <div className="topbar">
+            <div className="topbar__container">
+                <text className="topbar__login_text">Login</text>
+            </div>
+        </div>
+    )
+}
+
+export default TopBar;
